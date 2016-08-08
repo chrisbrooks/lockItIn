fe25d57c194c2af9539877ac0485f1c353954365
Jack Grigg <jgrigg@seek.com.au>
2016-08-08 17:43:43 +1000

    Added some debug script

--- agent/build.sh
+++ agent/build.sh
@@ -12,9 +12,7 @@ if [[ (! -d "$appSourceBaseDir/agent") || (! -d "$appSourceBaseDir/dist") ]]; th
   exitError "Script $(basename $0) must be run from the root of the project as agent/$(basename $0)"
 fi
 
-cat ~/.ssh/id_rsa
 SSH_KEY_BASE64=$(base64 ~/.ssh/id_rsa)
-cat $SSH_KEY_BASE64
 
 echo ">> Using docker version"
 docker version
