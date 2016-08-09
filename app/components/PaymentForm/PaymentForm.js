197cb9425bad7cfa624959807a95a658244e3f5d
Chris Brooks <c.brooks@seek.com.au>
2016-08-09 17:00:46 +1000

    webpack update

--- app/components/PaymentForm/PaymentForm.js
+++ app/components/PaymentForm/PaymentForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { PinkButton } from 'seek-style-guide/react';
 
 const PaymentForm = ({ onFormSChange, onFormSubmit, invoiceNumber, amount, loading }) => {
     return (
@@ -12,8 +11,6 @@ const PaymentForm = ({ onFormSChange, onFormSubmit, invoiceNumber, amount, loadi
                 <label htmlFor="amount">Amount</label>
                 <input type="number" id="amount" value={amount} onChange={onFormSChange} />
             </div>
-            <PinkButton type="submit"
-                        disable={loading}>{loading ? 'Processing...' : 'Submit'}</PinkButton>
         </form>
     );
 };
