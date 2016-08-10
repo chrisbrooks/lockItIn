266638b672fcfd246f255d5ddd2e88d6bd1d2482
Jack Grigg <jgrigg@seek.com.au>
2016-08-10 14:19:37 +1000

    Fixed cammel case stuff

---
+++ app/components/header/header.js
@@ -0,0 +1,33 @@
+import styles from './header.less';
+import React from 'react';
+import * as constants from '../../constants';
+
+const Header = ({ country }) => (
+    <div className={styles.header}>
+        <div className={styles.headerInnerContainer}>
+            <a href="/" className={styles.headerLogoContainer}>
+                <span className={styles.headerLogo}></span>
+                <span className={styles.headerLogoText}>employer</span>
+            </a>
+            <span className={styles.headerNumberContainer}>
+                <a href="/ContactUs" className={styles.headerNumber}>
+                    Customer Service
+                </a>&nbsp;
+                <i
+                    className={styles.headerIcon}
+                    aria-hidden="true"
+                    data-icon="&#57464;" />
+                <span className={styles.headerNumber} data-automation="headerNumber">
+                    {country === constants.location.AU ? constants.number.AU : constants.number.NZ}
+                </span>
+
+            </span>
+        </div>
+    </div>
+);
+
+Header.propTypes = {
+    country: React.PropTypes.string,
+};
+
+export default Header;
---
+++ app/components/paymentInfo/paymentInfo.js
@@ -0,0 +1,65 @@
+import styles from './paymentInfo.less';
+import React from 'react';
+
+const PaymentInfo = ({
+    invoiceNumber,
+    customerNumber,
+    amount,
+    surcharge,
+    cardType,
+    }) => {
+
+    let gst = amount * 10 / 100;
+    let surCharge = (surcharge / 100) * Number(amount);
+    let total = (gst + surCharge + Number(amount));
+
+    gst = gst.toFixed(2);
+    surCharge = surCharge.toFixed(2);
+    total = total.toFixed(2);
+
+    return (
+        <div className={styles.paymentInfo}>
+            <h2>Your payment information</h2>
+            <div className={styles.paymentInfoContainer}>
+                <p className={styles.paymentInfoTitle}>Invoice number</p>
+                <p className={styles.paymentInfoValue} data-automation="paymentInvoiceNumber">{invoiceNumber}</p>
+            </div>
+            <div className={styles.paymentInfoContainer}>
+                <p className={styles.paymentInfoTitle}>Customer number</p>
+                <p className={styles.paymentInfoValue} data-automation="paymentCustomerNumber">{customerNumber}</p>
+            </div>
+            <div className={styles.paymentInfoContainer}>
+                <p className={styles.paymentInfoTitle}>Invoice amount</p>
+                <p className={styles.paymentInfoValue}><span>$</span> {amount}</p>
+            </div>
+            <div className={styles.paymentInfoTotalContainer}>
+                <div className={styles.paymentInfoContainer}>
+                    <p className={styles.paymentInfoTitle}>Subtotal</p>
+                    <p className={styles.paymentInfoValue}><span>$</span> {amount}</p>
+                </div>
+                <div className={styles.paymentInfoContainer}>
+                    <p className={styles.paymentInfoTitle}>GST</p>
+                    <p className={styles.paymentInfoValue}><span>$</span>{gst}</p>
+                </div>
+                {surcharge > 0 && <div className={styles.paymentInfoContainer}>
+                    <p className={styles.paymentInfoTitle} data-automation="paymentSurchargeTitle">{cardType}</p>
+                    <p className={styles.paymentInfoValue} data-automation="paymentSurcharge"><span>$</span>{surCharge}</p>
+                </div>}
+                <div className={styles.paymentInfoContainer}>
+                    <p className={styles.paymentInfoTitle}>Total</p>
+                    <p className={styles.paymentInfoValue} data-automation="paymentTotal"><span>$</span>{total}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+PaymentInfo.propTypes = {
+    invoiceNumber: React.PropTypes.string,
+    customerNumber: React.PropTypes.string,
+    amount: React.PropTypes.string,
+    surcharge: React.PropTypes.number,
+    cardType: React.PropTypes.string,
+};
+
+export default PaymentInfo;
