7ea3ece51af211b90594148246abe087b6fbcf9c
Jack Grigg <jgrigg@seek.com.au>
2016-08-15 16:12:33 +1000

    debug

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
+console.log('process.env.AWS_ENVIRONMENT_NAME', process.env.AWS_ENVIRONMENT_NAME)
 if (process.env.AWS_ENVIRONMENT_NAME === 'production') {
+    console.log("HEEEEEERE production")
     module.exports = require('./config.prod.js');
 } else if (process.env.AWS_ENVIRONMENT_NAME === 'staging') {
+    console.log("HEEEEEERE staging")
     module.exports = require('./config.staging.js');
 } else {
     module.exports = require('./config.dev.js');
