33ed4a329cc4cf0a899fab916368355975bf82dc
Jack Grigg <jgrigg@seek.com.au>
2016-08-15 16:03:46 +1000

    Added stripe test publishable keys

--- config.staging.js
+++ config.staging.js
@@ -3,7 +3,7 @@ module.exports = {
     raygunIsEnabled: false,
     raygunApiKey: '',
     paymentUrl: 'http://talent.seek.com.au.staging/api/pay',
-    stripeAuPublishableKey: '',
-    stripeNzPublishableKey: '',
+    stripeAuPublishableKey: 'pk_test_BgjSlFxxesmRQfLEusmL62I4',
+    stripeNzPublishableKey: 'pk_test_DyIHZnE1y6PEsmvIRst7ZK1X',
     analyticsScriptPath: 'https://assets.adobedtm.com/prod.js',
 };
