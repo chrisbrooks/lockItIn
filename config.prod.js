784a3d33c870cd979d04999c891a5922fcbdee95
Chris Brooks <c.brooks@seek.com.au>
2016-08-22 09:50:58 +1000

    correcting raygun config

--- config.prod.js
+++ config.prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     environment: 'production',
-    raygunIsEnabled: false,
-    raygunApiKey: '',
+    raygunIsEnabled: true,
+    raygunApiKey: 'cu/2xo1uNrUlwdyd77yBnA==',
     paymentUrl: 'https://talent.seek.com.au/api/paymentservice/ChargePayOnline',
     stripeAuPublishableKey: 'pk_live_4xJnYi8Fnl0cXoE0EM3mwJFt',
     stripeNzPublishableKey: 'pk_live_lXounLQg840oL7Ud53RiqF2M',
--- config.staging.js
+++ config.staging.js
@@ -1,7 +1,7 @@
 module.exports = {
     environment: 'staging',
-    raygunIsEnabled: false,
-    raygunApiKey: '',
+    raygunIsEnabled: true,
+    raygunApiKey: 'cu/2xo1uNrUlwdyd77yBnA==',
     paymentUrl: 'https://talent.seek.com.au.staging/api/paymentservice/ChargePayOnline',
     stripeAuPublishableKey: 'pk_test_BgjSlFxxesmRQfLEusmL62I4',
     stripeNzPublishableKey: 'pk_test_DyIHZnE1y6PEsmvIRst7ZK1X',
