5dd6eef4a8a4cc04268c88138c9b3cf6664a7786
Chris Brooks <c.brooks@seek.com.au>
2016-08-24 12:07:37 +1000

    AK-723 moving agent folder

--- scripts/agent/build.sh
+++ scripts/agent/build.sh
@@ -6,6 +6,8 @@ DIRNAME=$(dirname $0)
 . "$DIRNAME/config"
 . "$DIRNAME/lib"
 
+echo "dsfsdfds $DIRNAME"
+
 # determine app source base directory - check for expected directories to confirm this
 appSourceBaseDir=$(pwd -P)
 if [[ (! -d "$appSourceBaseDir/scripts/agent") ]]; then
