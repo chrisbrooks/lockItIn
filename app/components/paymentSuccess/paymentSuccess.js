bbeb41903c435f99e1b01862402d3239796bc764
Chris Brooks <c.brooks@seek.com.au>
2016-08-25 16:06:59 +1000

    adding style changes

--- app/components/paymentSuccess/paymentSuccess.js
+++ app/components/paymentSuccess/paymentSuccess.js
@@ -14,28 +14,30 @@ const PaymentSuccess = ({
 
     return (
         <div className={styles.paymentSuccessPage}>
-            <h1 className={styles.paymentSuccessHeader}>Thanks. Your payment was successful.</h1>
-            <div className={styles.paymentSuccess}>
-                <h2 className={styles.paymentSuccessSubHeader}>Your payment information</h2>
-                <div className={styles.paymentSuccessContainer}>
-                    <p className={styles.paymentSuccessTitle}>Invoice number</p>
-                    <p className={styles.paymentSuccessValue} data-automation="paymentInvoiceNumber">{invoiceNumber}</p>
+            <div className={styles.paymentSuccessPageInner}>
+                <h1 className={styles.paymentSuccessHeader}>Thanks. Your payment was successful.</h1>
+                <div className={styles.paymentSuccess}>
+                    <h2 className={styles.paymentSuccessSubHeader}>Your payment information</h2>
+                    <div className={styles.paymentSuccessContainer}>
+                        <p className={styles.paymentSuccessTitle}>Invoice number</p>
+                        <p className={styles.paymentSuccessValue} data-automation="paymentInvoiceNumber">{invoiceNumber}</p>
+                    </div>
+                    <div className={styles.paymentSuccessContainer}>
+                        <p className={styles.paymentSuccessTitle}>Customer number</p>
+                        <p className={styles.paymentSuccessValue} data-automation="paymentCustomerNumber">{customerNumber}</p>
+                    </div>
+                    <div className={styles.paymentSuccessContainer}>
+                        <p className={styles.paymentSuccessTitle}>Payment Reference</p>
+                        <p className={styles.paymentSuccessValue} data-automation="paymentReference">{prn}</p>
+                    </div>
+                    <div className={styles.paymentSuccessContainer}>
+                        <p className={styles.paymentSuccessTitle}>Total Paid</p>
+                        <p className={styles.paymentSuccessValue} data-automation="paymentAmount">${totalAmount}</p>
+                    </div>
                 </div>
-                <div className={styles.paymentSuccessContainer}>
-                    <p className={styles.paymentSuccessTitle}>Customer number</p>
-                    <p className={styles.paymentSuccessValue} data-automation="paymentCustomerNumber">{customerNumber}</p>
-                </div>
-                <div className={styles.paymentSuccessContainer}>
-                    <p className={styles.paymentSuccessTitle}>Payment Reference</p>
-                    <p className={styles.paymentSuccessValue} data-automation="paymentReference">{prn}</p>
-                </div>
-                <div className={styles.paymentSuccessContainer}>
-                    <p className={styles.paymentSuccessTitle}>Total Paid</p>
-                    <p className={styles.paymentSuccessValue} data-automation="paymentAmount">${totalAmount}</p>
-                </div>
+                <p className={styles.paymentSuccessPrint}>Please <a onClick={print}>print this page</a> for your records</p>
+                <p className={styles.paymentSuccessPrintMobile}>A receipt has been sent to the billing address associated with this invoice.</p>
             </div>
-            <p className={styles.paymentSuccessPrint}>Please <a onClick={print}>print this page</a> for your records</p>
-            <p className={styles.paymentSuccessPrintMobile}>A receipt has been sent to the billing address associated with this invoice.</p>
         </div>
     );
 };
