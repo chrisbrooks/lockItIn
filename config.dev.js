fa53d74fd0574ef6f6642ed4d1794040b63c35ee
Chris Brooks <c.brooks@seek.com.au>
2016-10-11 17:06:10 +1100

    changing keys for stripe NZ test accoutn

--- config.dev.js
+++ config.dev.js
@@ -5,5 +5,5 @@ module.exports = {
     paymentUrlAu: 'http://localhost:2626/token/charge',
     paymentUrlNz: 'http://localhost:2626/token/charge',
     stripeAuPublishableKey: 'pk_test_BgjSlFxxesmRQfLEusmL62I4',
-    stripeNzPublishableKey: 'sk_test_G7k7oKq1Zb7zCJhuH55MG1yY',
+    stripeNzPublishableKey: 'pk_test_okMiZ2AVjfpw3PqMYlzNSFOT',
 };
--- config.staging.js
+++ config.staging.js
@@ -5,5 +5,6 @@ module.exports = {
     paymentUrlAu: 'https://talent.seek.com.au.staging/api/paymentservice/ChargePayOnline',
     paymentUrlNz: 'https://talent.seek.co.nz.staging/api/paymentservice/ChargePayOnline',
     stripeAuPublishableKey: 'pk_test_BgjSlFxxesmRQfLEusmL62I4',
-    stripeNzPublishableKey: 'sk_test_G7k7oKq1Zb7zCJhuH55MG1yY',
+    stripeNzPublishableKey: 'pk_test_okMiZ2AVjfpw3PqMYlzNSFOT',
+
 };
